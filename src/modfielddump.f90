fca1ab9061d13fc3ace3b2c0f2f58727601b4b3c
lorenzodonadio <lorenzodonadio@hotmail.com>
2025-01-30 11:33:46 +0100

    debug navg

--- src/modfielddump.f90
+++ src/modfielddump.f90
@@ -297,7 +297,9 @@ contains
 
       integer k,n
       ! we added 1 to the numberof observations in `vars`
+      write(*,*) "DEBUG addvarsnetcdf before increment: ", navg
       navg = navg + 1
+      write(*,*) "DEBUG addvarsnetcdf after increment: ", navg
 
       if (lu) vars(:,:,:,ind_u) = vars(:,:,:,ind_u) + u0(2:i1:ncoarse,2:j1:ncoarse,klow:khigh)
       if (lv) vars(:,:,:,ind_v) = vars(:,:,:,ind_v) + v0(2:i1:ncoarse,2:j1:ncoarse,klow:khigh)
