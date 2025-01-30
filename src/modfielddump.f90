a297d9b33533219f2f33bb04c56051ec7e781857
lorenzodonadio <lorenzodonadio@hotmail.com>
2025-01-30 12:06:03 +0100

    testdebug navg

--- src/modfielddump.f90
+++ src/modfielddump.f90
@@ -249,8 +249,13 @@ contains
       tnext = tnext+idtav
       dt_lim = minval((/dt_lim,tnext-timee/))
       ! Add variables and increase navg coun
-      if (lnetcdf .and. lsavetimeavg) call addvarsnetcdf
-      ! Only write fields if time is in the range (tmin, tmax)
+      if (lnetcdf .and. lsavetimeavg) then
+         write(*,*) "DEBUG addvarsnetcdf before increment: ", navg
+         navg = navg + 1
+         write(*,*) "DEBUG addvarsnetcdf after increment: ", navg
+         call addvarsnetcdf
+      endif
+          ! Only write fields if time is in the range (tmin, tmax)
       if (timee < itmin .or. timee > itmax) return
 
       if(lnetcdf) then
@@ -262,12 +267,15 @@ contains
          else
             call allocatevars
             call addvarsnetcdf
+            write(*,*) "DEBUG set navg to 1 before increment: ", navg
+            navg = 1
          endif
-
+         
          call writestat_nc(ncid,1,tncname,(/rtimee/),nrec,.true.)
          call writestat_nc(ncid,nvar,ncname,vars,nrec,ceiling(1.0*imax/ncoarse),ceiling(1.0*jmax/ncoarse),khigh-klow+1)
-
+         
          if (.not.lsavetimeavg) deallocate(vars)
+         write(*,*) "DEBUG navg to zerob4 : ", navg
          navg = 0
       end if
 
@@ -297,10 +305,7 @@ contains
 
       integer k,n
       ! we added 1 to the numberof observations in `vars`
-      write(*,*) "DEBUG addvarsnetcdf before increment: ", navg
-      navg = navg + 1
-      write(*,*) "DEBUG addvarsnetcdf after increment: ", navg
-
+      
       if (lu) vars(:,:,:,ind_u) = vars(:,:,:,ind_u) + u0(2:i1:ncoarse,2:j1:ncoarse,klow:khigh)
       if (lv) vars(:,:,:,ind_v) = vars(:,:,:,ind_v) + v0(2:i1:ncoarse,2:j1:ncoarse,klow:khigh)
       if (lw) vars(:,:,:,ind_w) = vars(:,:,:,ind_w) + w0(2:i1:ncoarse,2:j1:ncoarse,klow:khigh)
